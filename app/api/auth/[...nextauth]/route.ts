036868bb33b747a40220ff8b8e878c38d3ef0f31
rommel <rommel667@gmail.com>
2023-06-14 12:43:59 +0800

    setup nextauth with google and github provider

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,86 @@
+import { NextAuthOptions } from 'next-auth';
+import NextAuth from 'next-auth/next';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import GoogleProvider from 'next-auth/providers/google';
+import GithubProvider from 'next-auth/providers/github';
+import bcrypt from 'bcrypt';
+
+function getGoogleCredentials() {
+  const clientId = process.env.GOOGLE_CLIENT_ID;
+  const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
+  if (!clientId || clientId.length === 0) {
+    throw new Error('Missing GOOGLE_CLIENT_ID');
+  }
+  if (!clientSecret || clientSecret.length === 0) {
+    throw new Error('Missing GOOGLE_CLIENT_SECRET');
+  }
+  return { clientId, clientSecret };
+}
+
+function getGithubCredentials() {
+  const clientId = process.env.GITHUB_CLIENT_ID;
+  const clientSecret = process.env.GITHUB_CLIENT_SECRET;
+  if (!clientId || clientId.length === 0) {
+    throw new Error('Missing GITHUB_CLIENT_ID');
+  }
+  if (!clientSecret || clientSecret.length === 0) {
+    throw new Error('Missing GITHUB_CLIENT_SECRET');
+  }
+  return { clientId, clientSecret };
+}
+
+import prisma from '@/libs/prismadb';
+
+const handler: NextAuthOptions = NextAuth({
+  providers: [
+    CredentialsProvider({
+      name: 'Sign in',
+      credentials: {
+        email: {
+          label: 'Email',
+          type: 'email',
+          placeholder: 'example@example.com',
+        },
+        password: {
+          label: 'Password',
+          type: 'password',
+        },
+      },
+      async authorize(credentials, req) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error('Invalid credentials');
+        }
+
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.email },
+        });
+
+        if (!user || !user?.hashedPassword) {
+          throw new Error('Invalid credentials');
+        }
+
+        const isCorrectPassword = await bcrypt.compare(
+          credentials.password,
+          user.hashedPassword
+        );
+
+        if (!isCorrectPassword) throw new Error('Invalid credentials');
+
+        const { hashedPassword, ...userWithoutPassword } = user;
+
+        return userWithoutPassword;
+      },
+    }),
+    GoogleProvider({
+      clientId: getGoogleCredentials().clientId,
+      clientSecret: getGoogleCredentials().clientSecret,
+    }),
+    GithubProvider({
+      clientId: getGithubCredentials().clientId,
+      clientSecret: getGithubCredentials().clientSecret,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+});
+
+export { handler as GET, handler as POST };
