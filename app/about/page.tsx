eda8ede3bf0bbf36c83fc2367edffea43b101b77
rommel <rommel667@gmail.com>
2023-06-14 12:05:38 +0800

    navbar with link to pages

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+
+interface AboutProps {
+
+}
+
+const About: FC<AboutProps> = ({ }) => {
+    return <div>About Page</div>
+}
+
+export default About
---
+++ app/list/page.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+
+interface ListProps {
+
+}
+
+const List: FC<ListProps> = ({ }) => {
+    return <div>List Page</div>
+}
+
+export default List
