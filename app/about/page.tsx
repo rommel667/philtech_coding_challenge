6f98994064c55bfd8400e8271e214f02c7d0e367
rommel <rommel667@gmail.com>
2023-06-14 22:14:19 +0800

    update homepage

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -11,7 +11,12 @@ const About: FC<AboutProps> = ({ }) => {
             <h3>
                 Introducing VirtualList, the cutting-edge website that revolutionizes the way you organize and manage your data.
                 With VirtualList, you can say goodbye to the hassle of traditional lists and embrace the power of virtualized organization.
-                Our intuitive platform empowers you to effortlessly create, customize, and collaborate on lists of any size or complexity. Whether you're a busy professional tracking tasks, a meticulous planner organizing events, or a student managing assignments, VirtualList has got you covered. Our advanced features enable you to categorize, prioritize, and sort your data with ease, ensuring you stay on top of your game. Experience the freedom of accessing your lists anytime, anywhere, from any device, as VirtualList seamlessly syncs across all your devices. Boost your productivity, streamline your workflow, and reclaim control over your data with VirtualList. Join the virtualized list revolution today and unlock the true potential of efficient organization!
+                Our intuitive platform empowers you to effortlessly create, customize, and collaborate on lists of any size or complexity.
+                Whether you are a busy professional tracking tasks, a meticulous planner organizing events, or a student managing assignments,
+                VirtualList has got you covered. Our advanced features enable you to categorize, prioritize, and sort your data with ease,
+                ensuring you stay on top of your game. Experience the freedom of accessing your lists anytime, anywhere, from any device,
+                as VirtualList seamlessly syncs across all your devices. Boost your productivity, streamline your workflow, and reclaim control
+                over your data with VirtualList. Join the virtualized list revolution today and unlock the true potential of efficient organization!
             </h3>
 
         </div>
